d8cd63061e163d879e5380c14517b1e5e2018ea8
Nitesh Kumar <nitesh565golu@gmail.com>
2025-10-05 17:24:44 +0530

    last

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,20 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: import.meta.env.VITE_BACKEND_URL,
+// });
+
+// API.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("auth")
+//     ? JSON.parse(localStorage.getItem("auth")).token
+//     : null;
+//   if (token) {
+//     config.headers.Authorization = token;
+//   }
+//   return config;
+// });
+
+// export default API;
 import axios from "axios";
 
 const API = axios.create({
